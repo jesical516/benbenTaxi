00f70fedaf0f98399e51abe1cf0e649ca54fd2dd
jesical <chensongL516@gmail.com>
2013-08-04 15:49:40 +0800

    svn ci by chensong.zjd

---
+++ benbenTaxi/advertisingModel.h
@@ -0,0 +1,16 @@
+//
+//  AdvertisingModel.h
+//  benbenTaxi
+//
+//  Created by 晨松 on 13-8-4.
+//  Copyright (c) 2013年 晨松. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AdvertisingModel : NSObject {
+    bool status;
+    NSString* addvertisingInfo;
+}
+
+@end
---
+++ benbenTaxi/advertisingModel.m
@@ -0,0 +1,13 @@
+//
+//  AdvertisingModel.m
+//  benbenTaxi
+//
+//  Created by 晨松 on 13-8-4.
+//  Copyright (c) 2013年 晨松. All rights reserved.
+//
+
+#import "AdvertisingModel.h"
+
+@implementation AdvertisingModel
+
+@end
